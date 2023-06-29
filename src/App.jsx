9537d953ec66dc85c6f027154917bd3ec9797693
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2023-06-29 12:19:03 -0300

    Render cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,8 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Card } from "./components/Card/Card";
 
-const colorPerType = {
-  normal: "#A8A878",
-  fighting: "#C03028",
-  flying: "#A890F0",
-  poison: "#A040A0",
-  ground: "#E0C068",
-  rock: "#B8A038",
-  bug: "#A8B820",
-  ghost: "#705898",
-  steel: "#B8B8D0",
-  fire: "#FA6C6C",
-  water: "#6890F0",
-  grass: "#48CFB2",
-  electric: "#FFCE4B",
-  psychic: "#F85888",
-  ice: "#98D8D8",
-  dragon: "#7038F8",
-  dark: "#705848",
-  fairy: "#EE99AC",
-};
-
 function App() {
-  const [pokemon, setPokemon] = useState();
-  const [counter, setCounter] = useState(0);
+  const [pokemonList, setPokemonList] = useState();
 
   const fetchPokemon = async () => {
     // fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
@@ -50,7 +28,7 @@ function App() {
     const pokemonDetailedList = await Promise.all(promiseRequests);
 
     // Atualiza o estado dos pokemon
-    setPokemon(pokemonDetailedList);
+    setPokemonList(pokemonDetailedList);
   };
 
   useEffect(() => {
@@ -60,24 +38,9 @@ function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
-      <Card />
-      {pokemon?.map((pokemon) => {
-        // It will show only the color related to the first type
-        const [firstType] = pokemon.types;
-        const typeName = firstType.type.name;
-        return (
-          <div style={{ backgroundColor: colorPerType[typeName] }}>
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-            />
-            <button onClick={() => setCounter((old) => old+1)}>numero: {counter}</button>
-            <h2>{pokemon.name}</h2>
-            {pokemon.types.map(({ type }) => (
-              <h3>{type.name}</h3>
-            ))}
-          </div>
-        );
-      })}
+      {pokemonList?.map((pokemon) => (
+        <Card key={pokemon.id} pokemon={pokemon} />
+      ))}
     </div>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,47 @@
-export const Card = () => {
-  return <>card content</>
-}
+import { useState } from "react";
+
+const colorPerType = {
+  normal: "#A8A878",
+  fighting: "#C03028",
+  flying: "#A890F0",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  rock: "#B8A038",
+  bug: "#A8B820",
+  ghost: "#705898",
+  steel: "#B8B8D0",
+  fire: "#FA6C6C",
+  water: "#6890F0",
+  grass: "#48CFB2",
+  electric: "#FFCE4B",
+  psychic: "#F85888",
+  ice: "#98D8D8",
+  dragon: "#7038F8",
+  dark: "#705848",
+  fairy: "#EE99AC",
+};
+
+export const Card = (props) => {
+  const { pokemon } = props;
+
+  const [counter, setCounter] = useState(0);
+
+  // It will show only the color related to the first type
+  const [firstType] = pokemon.types;
+  const typeName = firstType.type.name;
+
+  return (
+    <div style={{ backgroundColor: colorPerType[typeName] }}>
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+      />
+      <button onClick={() => setCounter((old) => old + 1)}>
+        numero: {counter}
+      </button>
+      <h2>{pokemon.name}</h2>
+      {pokemon.types.map(({ type }) => (
+        <h3>{type.name}</h3>
+      ))}
+    </div>
+  );
+};
