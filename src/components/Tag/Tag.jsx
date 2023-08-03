f2dadf665d2df8a7eab6e30f1d8c43b6a515442e
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2023-08-03 10:07:02 -0300

    Install polished lib and apply style to tag component

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,11 +1,19 @@
+import { lighten } from "polished";
+
 export const Tag = (props) => {
   const { text } = props;
 
   return (
     <div
-      style={{ backgroundColor: "purple", borderRadius: 15, marginRight: 8 }}
+      style={{
+        backgroundColor: lighten(0.06, "rgb(72, 207, 178)"),
+        borderRadius: 15,
+        marginRight: 8,
+        padding: "4px 16px",
+        height: "fit-content",
+      }}
     >
-      <h3>{text}</h3>
+      <h3 style={{ margin: 0 }}>{text}</h3>
     </div>
   );
 };
