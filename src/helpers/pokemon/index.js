75052f6512d5b872296537b56d59e638884921a1
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2023-11-23 11:05:56 -0300

    create helper for hp stat

--- src/helpers/pokemon/index.js
+++ src/helpers/pokemon/index.js
@@ -1,2 +1,14 @@
 export const parseIdIntoPokedexNumber = (id) =>
-  `#${id.toString().padStart(3, "0")}`;
+  `#${id.toString().padStart(3, '0')}`;
+
+const calculateHp = (base, ev, iv) => {
+  return base * 2 + 10 + ev + iv + 100;
+};
+
+export const calculateMaxHp = (base) => {
+  return calculateHp(base, 63, 31);
+};
+
+export const calculateMinHp = (base) => {
+  return calculateHp(base, 0, 0);
+};
