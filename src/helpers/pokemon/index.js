0f84dc60394744c071c3f9ea2953db419a7c2158
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2023-12-05 17:12:17 -0300

    create helper for other stats

--- src/helpers/pokemon/index.js
+++ src/helpers/pokemon/index.js
@@ -12,3 +12,34 @@ export const calculateMaxHp = (base) => {
 export const calculateMinHp = (base) => {
   return calculateHp(base, 0, 0);
 };
+
+const calculateOtherStats = (base, ev, iv, nature) => {
+  return (base * 2 + 5 + ev + iv)*nature;
+};
+
+export const calculateMaxOtherStats = (base) => {
+  return calculateOtherStats(base, 63, 31,1.1);
+};
+
+export const calculateMinOtherStats = (base) => {
+  return calculateOtherStats(base, 0, 0,0.9);
+};
+
+export const parseStats = (stats) => {
+  const translateStatToCamelCase = {
+    hp: 'hp',
+    attack: 'attack',
+    defense: 'defense',
+    'special-attack': 'specialAttack',
+    'special-defense': 'specialDefense',
+    speed: 'speed',
+  };
+
+  return stats.reduce((acc, cur) => {
+    const { base_stat, stat } = cur;
+
+    const statName = translateStatToCamelCase[stat.name];
+
+    return { ...acc, [statName]: base_stat };
+  }, {});
+};
--- src/pages/PokemonDetail/PokemonDetail.jsx
+++ src/pages/PokemonDetail/PokemonDetail.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Card } from '../../components/Card/Card';
+import {
+  calculateMaxHp,
+  calculateMinHp,
+  parseStats,
+} from '../../helpers/pokemon';
 
 export const PokemonDetail = (props) => {
   const params = useParams();
@@ -20,9 +25,30 @@ export const PokemonDetail = (props) => {
     fetchPokemon();
   }, []);
 
+  if (!pokemonSelected) return <></>;
+  
+  const baseStats = parseStats(pokemonSelected.stats);
+  console.log(baseStats);
+
   return (
     <div>
       {pokemonSelected && <Card pokemon={pokemonSelected} />}
+      <h3>Base Stats</h3>
+      <h5>HP</h5>
+      <h5>{baseStats.hp}</h5>
+      <h5>{calculateMinHp(baseStats.hp)}</h5>
+      <h5>{calculateMaxHp(baseStats.hp)}</h5>
+      <h5>Attack</h5>
+      <h5>{baseStats.attack}</h5>
+      <h5>Defence</h5>
+      <h5>{baseStats.defence}</h5>
+      <h5>Sp. Attack</h5>
+      <h5>{baseStats.specialAttack}</h5>
+      <h5>Sp. Defence</h5>
+      <h5>{baseStats.specialDefence}</h5>
+      <h5>Speed</h5>
+      <h5>{baseStats.speed}</h5>
+      <h5>Total</h5>
     </div>
   );
 };
