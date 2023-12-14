feda433ad3254ee93a6a660c4b28be49a9d4218a
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2023-12-14 10:53:26 -0300

    create class and containers for all the stats

--- src/pages/PokemonDetail/PokemonDetail.jsx
+++ src/pages/PokemonDetail/PokemonDetail.jsx
@@ -40,74 +40,104 @@ export const PokemonDetail = (props) => {
       <h3>Base Stats</h3>
 
       <div>
-        <h5>HP</h5>
-        <h5>{baseStats.hp}</h5>
+        <h5 className="baseStatLabel">HP</h5>
+        <h5 className="baseStatLabel">{baseStats.hp}</h5>
         <progress
           style={{ '--color': color }}
           class="stats"
           value={baseStats.hp}
           max={calculateMaxHp(baseStats.hp)}
         />
-        <h5>{calculateMinHp(baseStats.hp)}</h5>
-        <h5>{calculateMaxHp(baseStats.hp)}</h5>
+        <h5 className="baseStatLabel">{calculateMinHp(baseStats.hp)}</h5>
+        <h5 className="baseStatLabel">{calculateMaxHp(baseStats.hp)}</h5>
       </div>
 
-      <h5>Attack</h5>
-      <h5>{baseStats.attack}</h5>
-      <progress
-        style={{ '--color': color }}
-        class="stats"
-        value={baseStats.attack}
-        max={calculateMaxOtherStats(baseStats.attack)}
-      />
-      <h5>{calculateMinOtherStats(baseStats.attack)}</h5>
-      <h5>{calculateMaxOtherStats(baseStats.attack)}</h5>
+      <div>
+        <h5 className="baseStatLabel">Attack</h5>
+        <h5 className="baseStatLabel">{baseStats.attack}</h5>
+        <progress
+          style={{ '--color': color }}
+          class="stats"
+          value={baseStats.attack}
+          max={calculateMaxOtherStats(baseStats.attack)}
+        />
+        <h5 className="baseStatLabel">
+          {calculateMinOtherStats(baseStats.attack)}
+        </h5>
+        <h5 className="baseStatLabel">
+          {calculateMaxOtherStats(baseStats.attack)}
+        </h5>
+      </div>
 
-      <h5>Defense</h5>
-      <h5>{baseStats.defense}</h5>
-      <progress
-        style={{ '--color': color }}
-        class="stats"
-        value={baseStats.defense}
-        max={calculateMaxOtherStats(baseStats.defense)}
-      />
-      <h5>{calculateMinOtherStats(baseStats.defense)}</h5>
-      <h5>{calculateMaxOtherStats(baseStats.defense)}</h5>
+      <div>
+        <h5 className="baseStatLabel">Defense</h5>
+        <h5 className="baseStatLabel">{baseStats.defense}</h5>
+        <progress
+          style={{ '--color': color }}
+          class="stats"
+          value={baseStats.defense}
+          max={calculateMaxOtherStats(baseStats.defense)}
+        />
+        <h5 className="baseStatLabel">
+          {calculateMinOtherStats(baseStats.defense)}
+        </h5>
+        <h5 className="baseStatLabel">
+          {calculateMaxOtherStats(baseStats.defense)}
+        </h5>
+      </div>
 
-      <h5>Sp. Attack</h5>
-      <h5>{baseStats.specialAttack}</h5>
-      <progress
-        style={{ '--color': color }}
-        class="stats"
-        value={baseStats.specialAttack}
-        max={calculateMaxOtherStats(baseStats.specialAttack)}
-      />
-      <h5>{calculateMinOtherStats(baseStats.specialAttack)}</h5>
-      <h5>{calculateMaxOtherStats(baseStats.specialAttack)}</h5>
+      <div>
+        <h5 className="baseStatLabel">Sp. Attack</h5>
+        <h5 className="baseStatLabel">{baseStats.specialAttack}</h5>
+        <progress
+          style={{ '--color': color }}
+          class="stats"
+          value={baseStats.specialAttack}
+          max={calculateMaxOtherStats(baseStats.specialAttack)}
+        />
+        <h5 className="baseStatLabel">
+          {calculateMinOtherStats(baseStats.specialAttack)}
+        </h5>
+        <h5 className="baseStatLabel">
+          {calculateMaxOtherStats(baseStats.specialAttack)}
+        </h5>
+      </div>
 
-      <h5>Sp. Defense</h5>
-      <h5>{baseStats.specialDefense}</h5>
-      <progress
-        style={{ '--color': color }}
-        class="stats"
-        value={baseStats.specialDefense}
-        max={calculateMaxOtherStats(baseStats.specialDefense)}
-      />
-      <h5>{calculateMinOtherStats(baseStats.specialDefense)}</h5>
-      <h5>{calculateMaxOtherStats(baseStats.specialDefense)}</h5>
+      <div>
+        <h5 className="baseStatLabel">Sp. Defense</h5>
+        <h5 className="baseStatLabel">{baseStats.specialDefense}</h5>
+        <progress
+          style={{ '--color': color }}
+          class="stats"
+          value={baseStats.specialDefense}
+          max={calculateMaxOtherStats(baseStats.specialDefense)}
+        />
+        <h5 className="baseStatLabel">
+          {calculateMinOtherStats(baseStats.specialDefense)}
+        </h5>
+        <h5 className="baseStatLabel">
+          {calculateMaxOtherStats(baseStats.specialDefense)}
+        </h5>
+      </div>
 
-      <h5>Speed</h5>
-      <h5>{baseStats.speed}</h5>
-      <progress
-        style={{ '--color': color }}
-        class="stats"
-        value={baseStats.speed}
-        max={calculateMaxOtherStats(baseStats.speed)}
-      />
-      <h5>{calculateMinOtherStats(baseStats.speed)}</h5>
-      <h5>{calculateMaxOtherStats(baseStats.speed)}</h5>
+      <div>
+        <h5 className="baseStatLabel">Speed</h5>
+        <h5 className="baseStatLabel">{baseStats.speed}</h5>
+        <progress
+          style={{ '--color': color }}
+          class="stats"
+          value={baseStats.speed}
+          max={calculateMaxOtherStats(baseStats.speed)}
+        />
+        <h5 className="baseStatLabel">
+          {calculateMinOtherStats(baseStats.speed)}
+        </h5>
+        <h5 className="baseStatLabel">
+          {calculateMaxOtherStats(baseStats.speed)}
+        </h5>
+      </div>
 
-      <h5>Total</h5>
+      <h5 className="baseStatLabel">Total</h5>
     </div>
   );
 };
